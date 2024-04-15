7a978df851bf330ec3d6b306b058c60e35fac104
dni ⚡ <office@dnilabs.com>
2024-04-15 13:10:11 +0200

    fix: properly start/stop extensions (#9)
    https://github.com/lnbits/lnbits/issues/2411

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 import asyncio
 
 from fastapi import APIRouter
+from loguru import logger
 
 from lnbits.db import Database
 from lnbits.helpers import template_renderer
-from lnbits.tasks import catch_everything_and_restart
+from lnbits.tasks import create_permanent_unique_task
 
 db = Database("ext_lnticket")
 
@@ -27,6 +28,17 @@ from .views import *  # noqa
 from .views_api import *  # noqa
 
 
+scheduled_tasks: list[asyncio.Task] = []
+
+
+def lnticket_stop():
+    for task in scheduled_tasks:
+        try:
+            task.cancel()
+        except Exception as ex:
+            logger.warning(ex)
+
+
 def lnticket_start():
-    loop = asyncio.get_event_loop()
-    loop.create_task(catch_everything_and_restart(wait_for_paid_invoices))
+    task = create_permanent_unique_task("ext_lnticket", wait_for_paid_invoices)
+    scheduled_tasks.append(task)
